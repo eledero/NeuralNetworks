ca99629a97c28749b2f1f9e872d02980ffd767cf
eledero <delarosaelias1@gmail.com>
2016-06-16 20:09:04 -0500

    favor

---
+++ mlclass-ex4/randInitializeWeights.m
@@ -0,0 +1,30 @@
+function W = randInitializeWeights(Lin, Lout)
+%RANDINITIALIZEWEIGHTS Randomly initialize the weights of a layer with L_in
+%incoming connections and L_out outgoing connections
+%   W = RANDINITIALIZEWEIGHTS(L_in, L_out) randomly initializes the weights 
+%   of a layer with L_in incoming connections and L_out outgoing 
+%   connections. 
+%
+%   Note that W should be set to a matrix of size(Lout, 1 + Lin) as
+%   the column row of W handles the "bias" terms
+%
+
+% You need to return the following variables correctly 
+W = zeros(Lout, 1 + Lin);
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Initialize W randomly so that we break the symmetry while
+%               training the neural network.
+%
+% Note: The first row of W corresponds to the parameters for the bias units
+%
+
+
+
+
+% Randomly initialize the weights to small values
+epsiloninit = 0.12;
+W = rand(Lout, 1 + Lin) * 2 * epsiloninit - epsiloninit;
+% =========================================================================
+
+end
